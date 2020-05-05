9a8e8c353b82751be376bf384107af902152ff54
letiziafara <61886833+letiziafara@users.noreply.github.com>
2020-05-05 16:26:22 -0700

    update
    einaudi website

--- assets/javascripts/nav_bar.js
+++ assets/javascripts/nav_bar.js
@@ -1,8 +1,14 @@
 function myFunction() {
   var x = document.getElementById("myTopnav");
+  var y = document.getElementById("myInnerTopnav");
+  var z = document.getElementById("myBannerText");
   if (x.className === "topnav") {
     x.className += " responsive";
-  } else {
+    y.className += " responsive";
+    z.className += " responsive";
+  }else {
     x.className = "topnav";
+    y.className = "topnav-right";
+    z.className = "banner-text";
   }
 }
